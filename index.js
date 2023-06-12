8548ad24015107aa049aca06c83c24bb4ff08e5b
asad2002web <asad2002web@gmail.com>
2023-06-12 22:51:05 +0600

    user data collection and api post added

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require("express");
-const cors = require("cors");
-const port = process.env.PORT || 4000;
 const app = express();
+const cors = require("cors");
 require("dotenv").config();
+const port = process.env.PORT || 4000;
 
 // middleware
 app.use(cors());
@@ -10,8 +10,8 @@ app.use(express.json());
 
 // Mongo DB
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://asad1234:asad1234@cluster0.ap8uoiz.mongodb.net/?retryWrites=true&w=majority";
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ap8uoiz.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -19,23 +19,41 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    // TODO: Start DB
+
+    const usersCollection = client.db("fluencyDB").collection("users");
+
+    // users api
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email };
+      const existingUser = await usersCollection.findOne(query);
+
+      if (existingUser) {
+        return res.send({ message: "user already exists" });
+      }
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
   }
 }
 run().catch(console.dir);
-
 
 app.get("/", (req, res) => {
   res.send("Fluency is running");
