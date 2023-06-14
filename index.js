1d23b89924aa74f84b163b73553f382c4a6557c2
asad2002web <asad2002web@gmail.com>
2023-06-15 00:21:30 +0600

    class api create

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const port = process.env.PORT || 4000;
 
@@ -8,9 +9,27 @@ const port = process.env.PORT || 4000;
 app.use(cors());
 app.use(express.json());
 
+// jwt verify function
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res.status(401).send({ error: true, message: "Access Denai" });
+  }
+  // bearer token
+  const token = authorization.split(" ")[1];
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_KEY, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ error: true, message: "Access Denai" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+};
+
 // Mongo DB
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ap8uoiz.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -29,8 +48,22 @@ async function run() {
     // TODO: Start DB
 
     const usersCollection = client.db("fluencyDB").collection("users");
+    const AddClassCollection = client.db("fluencyDB").collection("addClass");
 
+    // jwt api
+    app.post("/jwt", (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_KEY, {
+        expiresIn: "1h",
+      });
+      res.send({ token });
+    });
     // users api
+    app.get("/users", async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    });
+
     app.post("/users", async (req, res) => {
       const user = req.body;
       const query = { email: user.email };
@@ -40,6 +73,46 @@ async function run() {
         return res.send({ message: "user already exists" });
       }
       const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
+
+    app.patch("/users/admin/:id", async (req, res) => {
+      const id = req.params.id;
+
+      const { role } = req.body;
+
+      // Define your condition based on the role
+      let updatedRole;
+      if (role === "admin") {
+        updatedRole = "admin";
+      } else if (role === "instructor") {
+        updatedRole = "instructor";
+      } else {
+        return res.status(400).json({ error: "Invalid role" });
+      }
+
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: updatedRole,
+        },
+      };
+
+      try {
+        const result = await usersCollection.updateOne(filter, updateDoc);
+        res.json(result);
+      } catch (error) {
+        res.status(500).json({ error: "Failed to update user role" });
+      }
+    });
+    // Added Classes API
+     app.get("/addedClass", async (req, res) => {
+      const result = await AddClassCollection.find().toArray();
+      res.send(result);
+    });
+     app.post("/addedClass", async (req, res) => {
+      const addClass = req.body;
+      const result = await AddClassCollection.insertOne(addClass);
       res.send(result);
     });
 
